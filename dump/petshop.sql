7d13d65b16b188f3973a7cf9337a74369e1f78c9
it20181574 <149716721+it20181574@users.noreply.github.com>
2023-11-03 23:24:21 +0530

    Update petshop.sql

--- dump/petshop.sql
+++ dump/petshop.sql
@@ -1,11 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Nov 03, 2023 at 05:48 PM
+-- Server version: 5.7.28
+-- PHP Version: 8.2.8
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `petshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Person`
+--
 
 CREATE TABLE `Person` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `Person`
+--
 
 INSERT INTO `Person` (`id`, `name`) VALUES
 (1, 'Ranjit'),
@@ -13,16 +42,108 @@ INSERT INTO `Person` (`id`, `name`) VALUES
 (3, 'Dinesh'),
 (4, 'Karthik');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `desc` text NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `rrp` decimal(7,2) NOT NULL DEFAULT '0.00',
+  `quantity` int(11) NOT NULL,
+  `img` text NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `desc`, `price`, `rrp`, `quantity`, `img`, `date_added`) VALUES
+(1, 'dog', '', 29.99, 0.00, 10, 'dog.jpg', '2019-03-13 17:55:22'),
+(2, 'cat', '', 14.99, 19.99, 34, 'cat.jpg', '2019-03-13 18:52:49'),
+(3, 'Bird', '', 19.99, 0.00, 23, 'bird.jpg', '2019-03-13 18:47:56'),
+(4, 'fish', '', 69.99, 0.00, 7, 'fish.jpg', '2019-03-13 17:42:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
 
 CREATE TABLE `test` (
   `id` int(11) NOT NULL,
   `age` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `test`
+--
 
 INSERT INTO `test` (`id`, `age`) VALUES
 (1, '12'),
 (2, '14');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `users`
+--
 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `password`) VALUES
+(1, 'pet', 'pet@gmail.com', '0772289561', '$2y$10$b3Y7Rq5UOE0MCx9T66fB7Osji3oKyWPgHWSbftrKJ3VasiaK8iX1G'),
+(2, 'test', 'test@gmail.com', '0898989', '$2y$10$9YL6S8juoX/.hLIYznznvOaiGxpKKVHZVqyDUaajdkjSzoQ539cyq'),
+(3, 'Tharuka', 'Tharuka@gmail.com', '0710892586', '$2y$10$ZyJgC3.ZEXFu1wvtVT3DIOIcbjA6id2Xm8PxrgaRVPMYWIRpm.aL6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
